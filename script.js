1717bde2cc61bca4b1a8a0ea83aaddb68429342b
shweta604 <shwetapatil604@gmail.com>
2022-06-06 01:06:37 +0530

    change index and script

--- script.js
+++ script.js
@@ -1,58 +1,81 @@
+var movies = [];
 const form = document.querySelector('.form-container');
 
-const movieName = document.getElementById('movieName');
-const releasedYear = document.getElementById('releasedYear');
 
-const movieList = document.querySelector('.movie-list');
+window.onload = function() {
+    movies = JSON.parse(localStorage.getItem('movies'));
 
-// submit form to get the list item and append it in ul
+    // as soon as the page is loaded 
+    displayMoviesList();
+}
+
 form.addEventListener('submit', (e)=>{
     e.preventDefault();
-    // console.log('form submitted');
 
-    const movieNameInput = movieName.value;
-    const releasedYearInput = releasedYear.value;
+    const newMovie = {
+        movie: e.target.elements.movie.value,
+        year: e.target.elements.year.value
+    };
 
-    const movieListItem = document.createElement('li');
+    movies.push(newMovie);
 
-    movieListItem.innerHTML = `
-        <div class="movie-info">
-            <input type="text" value="${movieNameInput}" readonly>                
-            <input type="number" value="${releasedYearInput}" max="2022" min="1950" readonly>
-        </div>
+    localStorage.setItem('movies', JSON.stringify(movies));
 
-        <button class="update">Update</button>
-        <button class="delete">Delete</button>
-    `;
+    e.target.reset();
 
-    // console.log(movieListItem);
-    movieList.append(movieListItem);
-
-    movieName.value = '';
-    releasedYear.value = '';
+    displayMoviesList();
 });
 
-// use event delegation to get the specific element if the user clicked anywhere in the ul and perform the specific action
-movieList.addEventListener('click', (e)=>{
-    // console.log(e.target);
+function displayMoviesList(){
+    const ul = document.querySelector('.movie-list');
 
-    if(e.target.classList.contains('delete')){
-        const deleteMovie = e.target.parentElement;
-        // console.log(deleteMovie);
-        deleteMovie.remove();
-    } else if(e.target.classList.contains('update')){
-        const changeName = e.target.previousElementSibling.firstElementChild;
-        const changeYear = e.target.previousElementSibling.lastElementChild;
-        // console.log(changeName);
-        changeName.removeAttribute('readonly');
-        changeName.focus();
-        changeYear.removeAttribute('readonly');
+    ul.innerHTML = '';
 
-        document.addEventListener('keypress', (e)=>{
-            if(e.key == 'Enter'){
-                changeName.setAttribute('readonly', 'readonly');
-                changeYear.setAttribute('readonly', 'readonly');
-            }
-        })
-    } 
-})
+    movies.forEach(movieItem=>{
+        const li = document.createElement('li');
+
+        const infoDiv = document.createElement('div');
+        infoDiv.classList.add('movie-info');
+
+        const update = document.createElement('button');
+        update.classList.add('update');
+
+        const del = document.createElement('button');
+        del.classList.add('delete');
+
+        infoDiv.innerHTML = `
+            <input type="text" value="${movieItem.movie}" readonly>                
+            <input type="number" value="${movieItem.year}" max="2022" min="1950" readonly>`;
+
+        update.innerHTML = 'EDIT';
+        del.innerHTML = 'DELETE';
+
+        li.append(infoDiv);
+        li.append(update);
+        li.append(del);
+
+        ul.append(li);
+
+        update.addEventListener('click', e=>{
+            // console.log('update clicked');
+            const content = infoDiv.querySelectorAll('input').forEach(inputElement=>{
+                if(update.innerHTML == 'EDIT') {
+                    update.innerHTML = 'UPDATE';
+                    inputElement.removeAttribute('readonly');
+                    movieItem.inputElement = e.target.value;
+                    localStorage.setItem('movies', JSON.stringify(movies));
+                    displayMoviesList();
+                } else {
+                    inputElement.setAttribute('readonly', true);
+                    update.innerHTML = 'EDIT';
+                }
+            });
+        });
+
+        del.addEventListener('click', ()=>{
+            movies = movies.filter(movie => movie != movieItem);
+            localStorage.setItem('movies', JSON.stringify(movies));
+            displayMoviesList();
+        });
+    })
+}
